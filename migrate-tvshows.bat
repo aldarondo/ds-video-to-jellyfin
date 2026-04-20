9cdaeca9eaf51c526c0ed4d9c9a1ff503d3225f2
Charles Aldarondo <charles.aldarondo@gmail.com>
2026-04-20 04:25:23 -0700

    chore: Done. --overwrite removed. Steps when ready:  1. Remove TV S

--- migrate-tvshows.bat
+++ migrate-tvshows.bat
@@ -3,6 +3,5 @@ node "C:\Users\Aldarondo Family\Documents\Github\ds-video-to-jellyfin\dist\cli.j
   -i "V:\TV Shows" ^
   -o "V:\jellyfin\TV Shows" ^
   --hardlink ^
-  --overwrite ^
   --years-file "C:\Users\Aldarondo Family\Documents\Github\ds-video-to-jellyfin\show-years.json" ^
   >> "C:\Users\Aldarondo Family\Documents\Github\ds-video-to-jellyfin\migrate-tvshows.log" 2>&1
